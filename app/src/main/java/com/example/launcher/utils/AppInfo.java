d55b94e582b3bab2c5734a31dd90cbc21671cd50
myself <ans29hul@gmail.com>
2020-07-17 19:55:35 +0530

    home screen done

---
+++ app/src/main/java/com/example/launcher/utils/AppInfo.java
@@ -0,0 +1,11 @@
+package com.example.launcher.utils;
+
+import android.graphics.drawable.Drawable;
+
+public class AppInfo {
+    public String  label;
+    public String packageName;
+    public Drawable icon;
+
+
+}

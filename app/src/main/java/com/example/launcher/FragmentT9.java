cb389847db49eb9e6aafa88a6d0f54d8ecfa6c76
anshul_ubuntu <ans29hul@gmail.com>
2021-06-07 12:11:03 +0530

    updated

--- app/src/main/java/com/example/launcher/FragmentT9.java
+++ app/src/main/java/com/example/launcher/FragmentT9.java
@@ -12,8 +12,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -37,15 +38,31 @@ public class FragmentT9 extends Fragment implements View.OnClickListener {
     static String token;
     List<AppInfo> appList;
     Map<String,String> contactsInfoMap;
-    T9Trie<String> trie;
+    T9Trie<String> trie,app_trie,contact_trie;
     RecyclerView.Adapter adapter;
     List<AppInfo> newList;
+
+    Switch toggleSwitch;
+    static boolean toShowApp;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_c_layout, container, false);
 
         token="";
+
+        toggleSwitch=(Switch) view.findViewById(R.id.switch2);
+        toShowApp=true;
+        toggleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                toShowApp=!b;
+                updateListWith(token);
+            }
+        });
+
         ((Button)view.findViewById(R.id.b1)).setOnClickListener(this);
         ((Button)view.findViewById(R.id.b2)).setOnClickListener(this);
         ((Button)view.findViewById(R.id.b3)).setOnClickListener(this);
@@ -56,7 +73,9 @@ public class FragmentT9 extends Fragment implements View.OnClickListener {
         ((Button)view.findViewById(R.id.b8)).setOnClickListener(this);
         ((Button)view.findViewById(R.id.b9)).setOnClickListener(this);
         ((Button)view.findViewById(R.id.b0)).setOnClickListener(this);
+
         ((Button)view.findViewById(R.id.be3)).setOnClickListener(this);
+        //clear list on long press of clear button
         ((Button)view.findViewById(R.id.be3)).setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
@@ -71,7 +90,11 @@ public class FragmentT9 extends Fragment implements View.OnClickListener {
         appList=MainActivity.appData;
         contactsInfoMap=MainActivity.contactInfoMap;
 
-        trie=initTrie(appList,contactsInfoMap);
+        //to be removed
+        //trie=initTrie(appList,contactsInfoMap);
+
+        app_trie=initAppTrie(appList);
+        contact_trie=initContactTrie(contactsInfoMap);
 
         newList=new ArrayList<AppInfo>();
 
@@ -144,7 +167,7 @@ public class FragmentT9 extends Fragment implements View.OnClickListener {
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     private void makePhoneCallAt(final String token) {
-        ImageView b= getView().findViewById(R.id.be2);
+        ImageView b= getView().findViewById(R.id.icon3);
         String defaultDialer=((TelecomManager)getActivity().getSystemService(Context.TELECOM_SERVICE)).getDefaultDialerPackage();
         try {
             b.setImageDrawable(getActivity().getPackageManager().getApplicationIcon(defaultDialer));
@@ -164,62 +187,96 @@ public class FragmentT9 extends Fragment implements View.OnClickListener {
         });
     }
 
+
     @RequiresApi(api = Build.VERSION_CODES.M)
     private void updateListWith(String token) {
 
+        boolean isApp= toShowApp;
+
         String searchPrefix=token;
-        List<String> suggestions = trie.getT9ValueSuggestions(searchPrefix);
+        List<String> suggestions;
+
+        //change the icon of be2 button
+        ImageView icon1= getView().findViewById(R.id.icon1);
+        ImageView icon2= getView().findViewById(R.id.icon2);
+        ImageView icon3= getView().findViewById(R.id.icon3);
 
         //Generate a  list of AppData with string list
         newList.clear();
-        for (String s:suggestions) {
+        icon1.setImageDrawable(null);
+        icon2.setImageDrawable(null);
+        icon3.setImageDrawable(null);
 
-            if(MainActivity.appMap.containsKey(s))
-            {
-                //implies that the string is an app
-                String pkg=MainActivity.appMap.get(s);
-                Drawable icon=null;
-                try {
-                    icon = getActivity().getPackageManager().getApplicationIcon(pkg);
+        if(isApp){
+            suggestions = app_trie.getT9ValueSuggestions(searchPrefix);
 
-                }catch (Exception e)
-                {
+            for (String s:suggestions) {
+                    String pkg=MainActivity.appMap.get(s);
+                    Drawable icon=null;
+                    try {
+                        icon = getActivity().getPackageManager().getApplicationIcon(pkg);
 
+                    }catch (Exception e)
+                    {
+
+                    }
+                    newList.add(new AppInfo(s,pkg,icon));
+            }
+
+            icon1.setImageDrawable(newList.get(0).icon);
+            //add onclick listener
+            icon1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent launchIntent = getActivity().getPackageManager().getLaunchIntentForPackage(newList.get(0).packageName);
+                    startActivity(launchIntent);
                 }
-                newList.add(new AppInfo(s,pkg,icon));
+            });
+            if(newList.size()>1) {
+                icon2.setImageDrawable(newList.get(1).icon);
+                //add onclick listener
+                icon2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent launchIntent = getActivity().getPackageManager().getLaunchIntentForPackage(newList.get(1).packageName);
+                        startActivity(launchIntent);
+                    }
+                });
             }
-            else
-            {
-                //the string is a Contact
-                String number=MainActivity.contactInfoMap.get(s);
-                AppInfo info=new AppInfo();
-                info.isContact();
-                info.label=s;
-                info.contactNo=number;
-
-                newList.add(info);
-
+            if(newList.size()>2) {
+                icon3.setImageDrawable(newList.get(2).icon);
+                //add onclick listener
+                icon3.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent launchIntent = getActivity().getPackageManager().getLaunchIntentForPackage(newList.get(2).packageName);
+                        startActivity(launchIntent);
+                    }
+                });
             }
-
 
         }
+        else {
+            suggestions = contact_trie.getT9ValueSuggestions(searchPrefix);
+            for (String s:suggestions) {
 
-        //update recycler view
-        adapter.notifyDataSetChanged();
+                String number = MainActivity.contactInfoMap.get(s);
+                AppInfo info = new AppInfo();
+                info.isContact();
+                info.label = s;
+                info.contactNo = number;
 
-        //change the icon of be2 button
-        ImageView b= getView().findViewById(R.id.be2);
+                newList.add(info);
+            }
 
-        if(newList.get(0).isContact)
-        {
             String defaultDialer=((TelecomManager)getActivity().getSystemService(Context.TELECOM_SERVICE)).getDefaultDialerPackage();
             try {
-                b.setImageDrawable(getActivity().getPackageManager().getApplicationIcon(defaultDialer));
+                icon1.setImageDrawable(getActivity().getPackageManager().getApplicationIcon(defaultDialer));
 
             }
             catch (Exception e){}
             //add onclick listener
-            b.setOnClickListener(new View.OnClickListener() {
+            icon1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Intent call = new Intent(Intent.ACTION_CALL);
@@ -228,18 +285,10 @@ public class FragmentT9 extends Fragment implements View.OnClickListener {
                 }
             });
         }
-        else
-        {
-            b.setImageDrawable(newList.get(0).icon);
-            //add onclick listener
-            b.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent launchIntent = getActivity().getPackageManager().getLaunchIntentForPackage(newList.get(0).packageName);
-                    startActivity(launchIntent);
-                }
-            });
-        }
+
+
+        //update recycler view
+        adapter.notifyDataSetChanged();
 
 
     }
@@ -263,6 +312,40 @@ public class FragmentT9 extends Fragment implements View.OnClickListener {
             l.add(ci.getKey());
             trie.insert(ci.getKey(), l);
         }
+        trie.print();
+        return trie;
+    }
+
+    private T9Trie<String> initAppTrie(List<AppInfo> appList){
+
+        //String searchPrefix="42";
+
+        final T9Trie<String> trie = new T9Trie<>();
+        for (AppInfo app:appList)
+        {
+            LinkedList<String> l=new LinkedList<String>();
+            l.add(app.label);
+            trie.insert(app.label, l);
+        }
+
+        Log.d("Making app trie", "init ");
+        trie.print();
+        return trie;
+    }
+    private T9Trie<String> initContactTrie(Map<String,String> contactInfoMap){
+
+        //String searchPrefix="42";
+
+        final T9Trie<String> trie = new T9Trie<>();
+
+
+        for(Map.Entry<String,String> ci:contactInfoMap.entrySet())
+        {
+            LinkedList<String> l=new LinkedList<String>();
+            l.add(ci.getKey());
+            trie.insert(ci.getKey(), l);
+        }
+        Log.d("Making contact trie", "init ");
         trie.print();
         return trie;
     }
